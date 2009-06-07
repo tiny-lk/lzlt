09fb3fde132e408dd5ac9076e71d4a553eb31208
tiny.lk <tiny.lk@02f09b54-5303-11de-833c-a7bd19eed978>
2009-06-07 03:49:42 +0000

    基本mvc网站创建

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("LZL")]
+
+[assembly: AssemblyCopyright("Copyright © LZL 2009")]
+
+// 程序集的版本信息由下面四个值组成:
+//
+//      主版本
+//      次版本 
+//      内部版本号
+//      修订号
+//
+// 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
+// 方法是按如下所示使用“*”:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.*")]
---
+++ LZL.ForeignTrade.Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LZL.ForeignTrade.Site.MvcApplication" Language="C#" %>
