45ca95d9b99b1ec08d41bbcf58ad26d42acd7045
zhoubonet@gmail.com <zhoubonet@gmail.com@02f09b54-5303-11de-833c-a7bd19eed978>
2009-07-02 01:16:40 +0000

    修改客户查询功能样式，以及修复bug。

--- LZL.ForeignTrade.Controllers/CostomerController.cs
+++ LZL.ForeignTrade.Controllers/CostomerController.cs
@@ -8,6 +8,7 @@ using System.Configuration;
 using System.Data;
 using System.Data.Objects.DataClasses;
 using ZhouBo.Core;
+using System.Data.Objects;
 namespace LZL.ForeignTrade.Controllers
 {
     public class CostomerController : Controller
@@ -18,14 +19,43 @@ namespace LZL.ForeignTrade.Controllers
         /// <returns></returns>
         /// <param name="page">页码</param>
         /// <param name="w">查询条件</param>
-        public ActionResult Index(int? page, string w)
+        public ActionResult Index(int? page)
         {
             Entities _Entities = new Entities();
             int pagesize = int.Parse(ConfigurationManager.AppSettings["pagenumber"]);
-            var customers = _Entities.Customer.OrderBy(v => v.NameCode).Skip(pagesize * page ?? 0).Take(pagesize).ToList();
+            ViewData["pagecount"] = (int)Math.Ceiling((double)((double)Getcount(page, "NameCode", "")) / pagesize);
+            var customers = _Entities.Customer.OrderBy(v => v.NameCode).Skip(pagesize * ((page ?? 1) - 1)).Take(pagesize).ToList();
             return View(customers);
         }
 
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Index(string quyerCondition, string queryvalue)
+        {
+            Entities entities = new Entities();
+            int pagesize = int.Parse(ConfigurationManager.AppSettings["pagenumber"]);
+            ViewData["pagecount"] = (int)Math.Ceiling((double)((double)Getcount(1, quyerCondition, queryvalue)) / pagesize);
+            string sql = "select value it from " + entities.DefaultContainerName + ".Customer as it ";
+            if (!string.IsNullOrEmpty(queryvalue))
+            {
+                sql += " where it." + quyerCondition + " like '" + queryvalue + "%'";
+            }
+            sql += " order by it." + quyerCondition;
+            sql += " Skip " + 0 + " limit " + pagesize.ToString();
+            var querylist = entities.CreateQuery<Customer>(sql).ToList();
+            return View(querylist);
+        }
+
+        private int Getcount(int? page, string quyerCondition, string queryvalue)
+        {
+            Entities entities = new Entities();
+            string sql = "select value Count(it." + quyerCondition + ") from " + entities.DefaultContainerName + ".Customer as it ";
+            if (!string.IsNullOrEmpty(queryvalue))
+            {
+                sql += " where it." + quyerCondition + " like '" + queryvalue + "%'";
+            }
+            var count = entities.CreateQuery<int>(sql).FirstOrDefault();
+            return count;
+        }
         public ActionResult Add()
         {
             return View();
